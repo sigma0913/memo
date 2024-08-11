1735d866661a685d5939794a8b9af63d402dfba0
sigma0913 <sigma0913@outlook.jp>
2024-08-11 16:52:49 +0900

    +js

--- program.js
+++ program.js
@@ -5,6 +5,9 @@ let hairetsu = ['1','2','3','4','5','6','7','8']
 let index = 0
 
 while(index < 8){
-console.log(hairetsu[index])
+console.log(hairetsu[index]);
 index++;
 }
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
