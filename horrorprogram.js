36928cfc41ae1035aa0525863db0af0b6f70f275
sigma0913 <sigma0913@outlook.jp>
2024-08-12 18:42:50 +0900

    即時間数で囲う(()=>{...})();

--- horrorprogram.js
+++ horrorprogram.js
@@ -1,20 +1,22 @@
-let now = 0;
-let max = 0;
-
-const $dot = (arg) => {
-now = 0;
-    while(now < arg){
-        window.alert('...');
-        now++;
+(()=>{
+    let now = 0;
+    let max = 0;
+    
+    const $dot = (arg) => {
+    now = 0;
+        while(now < arg){
+            window.alert('...');
+            now++;
+        };
     };
-};
-
-const $air = (arg) => {
-document.getElementsByTagName('li')[arg].textContent = '';
-}
-
-document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
-    max++;
-    $dot(max);
-    $air(max-1);
-});
+    
+    const $air = (arg) => {
+    document.getElementsByTagName('li')[arg].textContent = '';
+    }
+    
+    document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
+        max++;
+        $dot(max);
+        $air(max-1);
+    });
+})
